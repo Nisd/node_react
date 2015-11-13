4e1c925cc1c4092aa78bd1f731ca2d76b17ce500
袁康 <yuankang@meituan.com>
2015-11-13 15:42:33 +0800

    updates

--- started/config/webpack.config.js
+++ started/config/webpack.config.js
@@ -5,6 +5,8 @@
 var path = require('path');
 var node_modules = path.resolve(__dirname, '../node_modules');
 
+//npm run dev:the bundle.js is serving at http://localhost:8080/assets/bundle.js
+
 module.exports = {
     entry: path.resolve(__dirname, '../src/client/scripts/client.js'),
     output: {
